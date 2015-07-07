9f41aaf4328cc505ae6585493d17738476ecf4a3
Lee Sangwon <leesn@bookp.al>
2015-07-08 08:48:35 +0900

    package description updated

--- package.js
+++ package.js
@@ -1,14 +1,12 @@
 Package.describe({
   name: 'leesangwon:logger',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '0.1.0',
+  summary: 'A simple Meteor logger that works on the server',
+  git: 'https://github.com/miraten/meteor-logger',
   documentation: 'README.md'
 });
+
+Npm.depends({ winston: "1.0.1" });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');

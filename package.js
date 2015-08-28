dd79d148c665ff573641d117d2cc9943d0ff2a90
Lee Sangwon <leesn@bookp.al>
2015-08-29 07:46:21 +0900

    updated to 0.2.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'leesangwon:logger',
-  version: '0.1.0',
+  version: '0.2.4',
   summary: 'A simple Meteor logger that works on the server',
   git: 'https://github.com/miraten/meteor-logger',
   documentation: 'README.md'
@@ -10,7 +10,9 @@ Npm.depends({ winston: "1.0.1" });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('logger.js');
+  api.addFiles('logger.js', 'server');
+
+  api.export('Logger', 'server');
 });
 
 Package.onTest(function(api) {
